a36e2acc22337b649644244622bd2dd404d58b09
Blake <blakeemersonfox@outlook.com>
2020-02-24 16:48:07 -0600

    This is a test. Please standby.

--- GameTester.java
+++ GameTester.java
@@ -1,33 +1,16 @@
 
 /**
- * Write a description of class GameTester here.
+ * Tester of the Peg Board Game
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Blake Fox
+ * @version 2/24/2020
  */
 public class GameTester
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class GameTester
-     */
-    public GameTester()
+    
+    public static void main(String[] args)
     {
-        // initialise instance variables
-        x = 0;
+        
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
