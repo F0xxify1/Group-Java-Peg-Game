dd8181ef068c21eb71a168b278f01dec3e99bdf8
Blake <blakeemersonfox@outlook.com>
2020-02-25 11:16:48 -0600

    Update

--- Graphics.java
+++ Graphics.java
@@ -8,28 +8,37 @@ import javax.swing.*;
  */
 public class Graphics
 {
-    JPanel panelc = new JPanel();
+    JFrame frame = new JFrame("Peg Game");
     private JButton[][] t = new JButton[9][9]; //Declared much earlier in the program, right after the class declaration.
     
     public Graphics()
     {
-        
+        frame.setSize(900, 700);
     }
     
     public void buildTile()
     {
-        panelc.setLayout(new GridLayout(9, 9));
-        for(int r = 0; r < 9; r++)
+        int row = 4;
+        int col = 7;
+        frame.setLayout(new GridLayout(row, col));
+        for(int r = 0; r < row; r++)
         {
-            for(int c = 0; c < 9; c++)
+            for(int c = 0; c < col; c++)
             {
-                t[r][c] = new JButton("O");
-                t[r][c].setBackground(Color.BLACK);
-                t[r][c].setForeground(Color.WHITE);
-                t[r][c].addActionListener(new TileListener());
-                panelc.add(t[r][c]);
+                t[c][r] = new JButton("");
+                t[c][r].setBackground(Color.BLACK);
+                t[c][r].setForeground(Color.WHITE);
+                t[c][r].addActionListener(new TileListener());
+                t[c][r].setBorderPainted(false);
+                frame.add(t[c][r]);
             }
         }
+        toggleVisible();
+    }
+    
+    public void toggleVisible()
+    {
+        frame.setVisible(!frame.isVisible());
     }
     
 }
--- TileListener.java
+++ TileListener.java
@@ -1,9 +1,14 @@
+import java.awt.*;
 import java.awt.event.*;
-
+import javax.swing.*;
 public class TileListener implements ActionListener
     {
         public void actionPerformed(ActionEvent e)
         {
-            //Some code to change a specific button
+            JButton temp = (JButton) e.getSource();
+            if (temp.getBackground() == Color.yellow)
+                temp.setBackground(Color.BLACK);
+            else
+                temp.setBackground(Color.yellow);
         }
     }
