685795d4b8b7cab1f9c6eff67f5e570088a79609
Cooper <chornstrom@gmail.com>
2020-03-04 09:33:37 -0600

    03/04/2020

--- Board.java
+++ Board.java
@@ -9,7 +9,7 @@ import java.awt.*;
  */
 public class Board
 {
-    
+    int[][] board;
     /**
      * Constructor for objects of class Board
      */
@@ -17,11 +17,24 @@ public class Board
     {
     }
     
-    boolean[] row1 = new boolean[1];
-    boolean[] row2 = new boolean[2];
-    boolean[] row3 = new boolean[3];
-    boolean[] row4 = new boolean[4];
-    boolean[] row5 = new boolean[5];
-   
+    public void newBoard(int rows)
+    {
+       int[][] pos = new int[rows + 2][rows + 2];
+       board = pos;
+        if(rows < 5)
+       {
+         System.out.print("invalid input");
+       }
+       else
+       {
+          for(int a = 1; a <= rows; a++)
+          {
+             for(int b = 1; b <= a; b++)
+             {
+               pos[b][a] = 1;
+             }
+          }
+       }
+    }
     
 }
